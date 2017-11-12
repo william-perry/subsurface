34b0069a02a8987d58389af41dc007db585219dd
Dirk Hohndel <dirk@hohndel.org>
2017-11-12 10:50:22 -0800

    Travis: create our MXE setup
    This branch does NOT build Subsurface.
    It's part of the Subsurface repo for convenience reasons.
    All it is used for is to create our own MXE environment which we can
    then use for builds of Subsurface for MXE on other branches.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ scripts/windows/create-mxe.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+cd ${TRAVIS_BUILD_DIR}/..
+if [ ! -d mxe/.git ] ; then
+	git clone git://github.com/mxe/mxe
+fi
+cd mxe
+git checkout 994ad   # this is the tested working version
+
+# create our config file
+/usr/bin/cat <<EOT > settings.mk
+JOBS := 4
+MXE_TARGETS := i686-w64-mingw32.shared
+LOCAL_PKG_LIST := libxml2 libxslt libusb1 qt5 nsis libzip
+.DEFAULT local-pkg-list:
+local-pkg-list: $(LOCAL_PKG_LIST)
+EOT
+
+/usr/bin/cat settings.mk
+
+make -j 4
+

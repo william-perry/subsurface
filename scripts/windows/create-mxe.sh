69a3ddcf7a0335c7b29389f989d8691d2ad5d13f
Dirk Hohndel <dirk@hohndel.org>
2017-11-12 18:37:06 -0800

    fix here document
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/windows/create-mxe.sh
+++ scripts/windows/create-mxe.sh
@@ -10,7 +10,7 @@ cd mxe
 git checkout 994ad   # this is the tested working version
 
 # create our config file
-/usr/bin/cat <<EOT > settings.mk
+/usr/bin/cat <<'EOT' > settings.mk
 JOBS := 4
 MXE_TARGETS := i686-w64-mingw32.shared
 LOCAL_PKG_LIST := libxml2 libxslt libusb1 qt5 nsis libzip
